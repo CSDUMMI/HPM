d78f070839992d25d4783ebf650e4a6bfcc0ba73
Joris Gutjahr <joris.gutjahr@gmail.com>
2019-01-07 18:42:51 +0100

    Build can now build simple Haskell Programms with ghc --make at its core

--- HPM/Build.hs
+++ HPM/Build.hs
@@ -2,6 +2,16 @@ module HPM.Build
     (
      build
      ) where
-
+import Data.List
+import System.Process as Process
 build :: [String] -> IO ()
-build x = undefined
+build [entryFile] = build [entryFile,entryFile,"."]
+build [entryFile,output] = build [entryFile,output,"."]
+build [entryFile,output,root] = do
+  let fullPath = root ++ "/" ++ entryFile
+  mapM putStr $ ["Building: ", fullPath, " as ", output, "\n"]
+  putStrLn "With:"
+  Process.runCommand "ghc --version"
+  putStr "\n\n"
+  Process.callCommand $ "ghc --make " ++ fullPath ++ " -o " ++ output
+  return ()
