f3a8fd25dd77f376bfe03525c00634e3e96d4407
rexim <reximkut@gmail.com>
2019-01-08 17:02:37 +0100

    Implemented and Documented hpm view, It now prints the Libaries (if there are any) and the Project.hpm file (if there is one)

--- HPM/View.hs
+++ HPM/View.hs
@@ -4,10 +4,21 @@ module HPM.View
   ) where
 
 -- Base Modules
-
+import Data.List
+import qualified System.Directory as Directory
 -- Own Modules
+import Config.HPMN (delimnatorHPMN)
 
+--Too many ifs I know
 view :: [String] -> IO ()
-view (root) = do
-  -- TODO : Check what Libaries are installed in root
-  --        and later perhapse print some more information
+view (root) =
+         do
+           -- Find archivar
+           if Directory.doesFileExist $ root ++ ".hpm/archiver"
+              then putStr $ readFile $ root ++ ".hpm/archiver"
+              else putStrLn " Checked: No Libaries installed here "
+
+           -- Display Project File (Project.hpm)
+           if Directory.doesFileExist $ root ++ "/Project.hpm"
+              then mapM_ putStrLn $ lines $ readFile $ root ++ "Project.hpm"
+              else putStrLn " Checked: No Project File avaible "
